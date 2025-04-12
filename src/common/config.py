c7c2541e31a320ba684ff13a6f12edd534e19fb5
myeongseok <myeongseok@kaist.ac.kr>
2025-04-12 00:07:24 -0400

    modify config py

--- src/common/config.py
+++ src/common/config.py
@@ -1,6 +1,6 @@
 import os
 
-AWS_REGION = os.environ.get("AWS_REGION", "us-east-2")
+AWS_REGION = os.environ.get("MY_REGION", "us-east-2")
 
 # 테이블 이름들도 환경변수로 빼두면 나중에 쉽게 변경 가능
 STORING_ORDERS_TABLE = os.environ.get("STORING_ORDERS_TABLE", "StoringOrders")
