7458700d1bdcd235d9efc2319596e07b21d6a103
myeongseok <myeongseok@kaist.ac.kr>
2025-04-12 01:09:42 -0400

    CORS allow

--- src/common/utils.py
+++ src/common/utils.py
@@ -5,6 +5,9 @@ def make_response(status_code: int, body: dict):
         "statusCode": status_code,
         "body": json.dumps(body),
         "headers": {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS"
         }
     }
