ef474a6b840b8337d9b7ad5319a1c276fea6410a
myeongseok <myeongseok@kaist.ac.kr>
2025-05-01 14:40:20 -0400

    modify backend

--- src/common/utils.py
+++ src/common/utils.py
@@ -1,13 +1,23 @@
 import json
 
-def make_response(status_code: int, body: dict):
-    return {
-        "statusCode": status_code,
-        "body": json.dumps(body),
-        "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-            "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS"
-        }
+def make_response(status_code: int, body: dict = None):
+    headers = {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,x-api-key",
+        "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+        "Access-Control-Max-Age": "600"
     }
+    
+    response = {
+        "statusCode": status_code,
+        "headers": headers
+    }
+    
+    if body is not None:
+        response["body"] = json.dumps(body)
+    
+    return response
+
+def handle_options():
+    return make_response(200)
