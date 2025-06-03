cddbc0e265d4ab1ce27a69b406c04237474b392e
myeongseok <myeongseok@kaist.ac.kr>
2025-06-03 17:20:12 -0400

    modify api format

--- src/read_storing_orders/app.py
+++ src/read_storing_orders/app.py
@@ -3,18 +3,17 @@ from boto3.dynamodb.conditions import Key
 from common.utils import storing_table, respond
 
 def lambda_handler(event, context):
-    auth = event['requestContext']['authorizer']
-    role = auth['role']
-    eid  = auth['employee_id']
+    auth = event['requestContext'].get('authorizer', {})
+    eid  = auth.get('employee_id')
 
-    if role == 'receiver':
+    if auth.get('role') == 'receiver':
         # only your own orders
         resp = storing_table.query(
             IndexName='ReceiverIndex',
             KeyConditionExpression=Key('receiver_id').eq(eid)
         )
         items = resp.get('Items', [])
-    elif role == 'admin':
+    elif auth.get('role') == 'admin':
         items = storing_table.scan().get('Items', [])
     else:
         return respond(403, {'message':'Forbidden'})
